25b081fa127eecbf036c62a10ac8a260010c1d26
Hemistone <kei6040@gmail.com>
2019-09-18 01:40:24 +0900

    final push

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-import Landing from "../Routes/Landing";
 import Console from "../Routes/Console";
 
 export default () => (
   <Router>
-    <Route exact path="/" component={Landing} />
-    <Route exact path="/console" component={Console} />
+    <Route exact path="/" component={Console} />
   </Router>
 );
